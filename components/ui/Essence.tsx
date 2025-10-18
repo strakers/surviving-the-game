4ea62612a0f1bf5c99dfe3b5c4c65b28b400878f
strakers <s.straker@utoronto.ca>
2025-10-18 13:02:46 -0400

    Update components to allow for stat processing and display without yaml formatting

--- components/ui/Essence.tsx
+++ components/ui/Essence.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren, type DetailedReactHTMLElement } from "react";
 import Spoiler from "@/components/ui/Spoiler";
 import { Card } from "fumadocs-ui/components/card";
 import {Tab, Tabs} from 'fumadocs-ui/components/tabs';
-import { twMerge as cn } from 'tailwind-merge';
+// import { twMerge as cn } from 'tailwind-merge';
 
 interface EssenceProps {
   name: string;
@@ -11,26 +11,76 @@ interface EssenceProps {
   chapterRemoved?: number;
 }
 
+type ChildHasType = {
+  type: (({ children }: { children?: React.ReactNode; }) => React.ReactNode),
+}
+
+type ChildPropsAsString = {
+  type: string,
+  props: {
+    children: string,
+  }
+}
+
+type StatPair = { name: string, value: number };
+
 const Abilities = ({ children }: PropsWithChildren) => {
   return children;
 }
 
-const Stats = ({ children }: PropsWithChildren) => {
-  return children;
+const Stats = ({ children }: PropsWithChildren<{ wrap: boolean }>) => {
+  // prepare variable for storing processed stats
+  const processedChildren: StatPair[] = [];
+
+  // extract processed stats from children if not Element
+  React.Children.toArray(children).forEach((child, i) => {
+    if (React.isValidElement(child)) {
+      if (assertChildPropsAsString(child)) {
+        const rawListString = child.props.children;
+        const compiledList: { [key: string]: number } = {};
+        rawListString.split("\n").forEach((pair:string) => {
+          const [name = '', value = ''] = pair.split(':').map((text: string) => text.trim());
+          compiledList[name] = Number(value);
+          processedChildren.push({ name, value: Number(value) })
+        });
+      }
+    }
+  });
+
+  // if processed, display individual stats
+  if (processedChildren.length) {
+    return <div data-processed="true">
+      {processedChildren.map(({ name, value }: StatPair, i) => (
+        <Stat key={i} name={name} value={value} />
+      ))}
+    </div>
+  }
+
+  // otherwise display children
+  return <div>{children}</div>;
+}
+
+function Stat({ name, value }: PropsWithChildren<StatPair>) {
+  return (
+    <div>
+      <span>{name}</span>:&nbsp;
+      <strong>{value > 0 ? '+' + value : value}</strong>
+    </div>
+  );
 }
 
 const validTabTypes = [Abilities, Stats];
 
 function Essence({ name, rank, chapterGained = -1, chapterRemoved = 0, children }: PropsWithChildren<EssenceProps>) {
-  const abilities = [];
-  const stats = [];
+  const abilities: React.ReactNode[] = [];
+  const stats: React.ReactNode[] = [];
   const tabLabels = [
     'Description',
   ];
 
   const newChildren = React.Children.map(children, child => {
     if (!React.isValidElement(child)) return child;
-    if (!validTabTypes.includes(child.type)) return child;
+    if (!assertChildHasType(child) || !validTabTypes.includes(child.type)) return child;
 
     switch (child.type) {
       case Abilities: abilities.push(<Tab value={'Abilities'}>{child}</Tab>); break;
@@ -63,6 +113,16 @@ function Essence({ name, rank, chapterGained = -1, chapterRemoved = 0, children 
 
     </Card>
   </Spoiler>);
+}
+
+function assertChildPropsAsString(value: {type?: unknown}): value is ChildPropsAsString {
+  if (value.type === undefined || typeof value.type !== 'string') return false;
+  // if (value.props === undefined || value.props.children === undefined || typeof value.props.children !== 'string') return false;
+  return true;
+}
+
+function assertChildHasType(value: { type?: unknown }): value is ChildHasType {
+  return (value.type !== undefined);
 }
 
 export { Essence, Abilities, Stats };
