fa6c76d0d481654ef5e77ee8f56f3638c39cc14b
strakers <contact@strakez.com>
2025-10-20 03:48:40 -0400

    Update the landing pages with STGB intro content

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">View the Wiki</h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
@@ -12,7 +12,7 @@ export default function HomePage() {
         >
           /docs
         </Link>{' '}
-        and see the documentation.
+        and see the documents.
       </p>
     </main>
   );
